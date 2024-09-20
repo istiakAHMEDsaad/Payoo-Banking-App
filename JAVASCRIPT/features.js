686c9d10ee4569f91305842c6aea54e46ca9f173
istiakAHMEDsaad <istik2020@gmail.com>
2024-09-20 16:40:46 +0600

    bug fix

--- JAVASCRIPT/features.js
+++ JAVASCRIPT/features.js
@@ -8,12 +8,14 @@ document.getElementById('button-one').addEventListener('click', function () {
   document.getElementById('add-mony-section').classList.remove('hidden');
   document.getElementById('cashout-section').classList.add('hidden');
   document.getElementById('default-pic').classList.add('hidden');
+  document.getElementById('coming-soon').classList.add('hidden');
 });
 // Show cashout
 document.getElementById('button-two').addEventListener('click', function () {
   document.getElementById('cashout-section').classList.remove('hidden');
   document.getElementById('add-mony-section').classList.add('hidden');
   document.getElementById('default-pic').classList.add('hidden');
+  document.getElementById('coming-soon').classList.add('hidden');
 });
 // Show working in progres button 3
 document.getElementById('button-three').addEventListener('click', function () {
